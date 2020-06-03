d06fe9d04ff1ad40523e5f9b33afad4619a862c7
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-06-03 18:58:04 -0400

    let vs var

--- 01-basic/js/app.js
+++ 01-basic/js/app.js
@@ -4,4 +4,30 @@ message = "I'm one of the survivors of the apocalypse of June 27, 2020";
 console.log(message);
 message = 'Dios mio no recordaba Javascript, que tonto.';
 console.log(message);
+message = 2020
+console.log(message);
 
+const msg = 'Hello';
+console.log(msg);
+
+let name;
+console.log(name);
+name = 'Walter White';
+console.log(name);
+
+var message = "Say my name.";
+console.log('1. Walter say: ', message);
+if (message){
+    var message = 'I am not in danger Skyler, I am the danger.';
+    console.log('2. Walter say: ', message);
+}
+console.log('3. Walter say: ', message);
+
+
+let walterMessage  = "I am the who knocks.";
+console.log('1. Heisenberg say: ', walterMessage);
+if (walterMessage){
+    let walterMessage = 'I am not in danger Skyler, I am the danger.';
+    console.log('2. Heisenberg say: ', walterMessage);
+}
+console.log('3. Heisenberg say: ', walterMessage);
